022d712947abd71495eab3d5ea05468643490ed8
Marcelo Rodrigues <manoelmarcelo.r@gmail.com>
2022-06-13 16:13:07 -0300

    add aula 11

---
+++ Aula 11 - alert, confirm e prompt(Navegador)/js/index.js
@@ -0,0 +1,9 @@
+alert('Olá Mundo - mensagem');
+
+// Manipulando Window
+// ok
+window.confirm('Deseja realmente apagar?');// tipo de alert com dois botoẽs retorna true ou false
+//window.prompt('Digite seu Nome');
+
+const test = prompt('Digite seu nome');
+alert('Bem vindo a JS ' + test)
