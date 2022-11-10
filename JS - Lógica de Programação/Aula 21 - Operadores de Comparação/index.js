202fb32710a2053aacd0356bb49c27983b18da70
Marcelo Rodrigues <85708828+MMarcelo-Rodrigues@users.noreply.github.com>
2022-11-09 22:30:38 -0300

    Nova estrutura do Curso JS

---
+++ JS - Lógica de Programação/Aula 21 - Operadores de Comparação/index.js
@@ -0,0 +1,10 @@
+/*   OPERADORES DE COMPARAÇÂO
+ 
+    >       maior que
+    >=      maior que ou igual a
+    <       menor que
+    <=      menor que ou igual a
+    ==      igualdade
+    ===     igualdade estrita
+
+ */
